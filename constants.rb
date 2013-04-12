c27ac838144768d946d26bc5613b785d3a58cc96
Pradeep Sethi <psethi@gmail.com>
2013-04-12 19:34:34 +0530

    removed private files

--- constants.rb
+++
@@ -1,8 +0,0 @@
-
-# This is the key & secret received from Twitter
-TWEEBOOSTER_DATA =
-  {  
-      :consumer_key => "hDj7zTiP4yVMdrc81VLxKA",
-      :consumer_secret => "C5UoYY9Vm0CnHByIuSBoZmYpfVjrBC25JsTqqVbjQ",
-}
-      
